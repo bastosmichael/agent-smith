a1fdc6f185fd17b69f253b68ea69f1ab1eede2ea
Michael Bastos <bastosmichael@gmail.com>
2018-11-15 17:19:54 -0600

    moving files by name complete

--- lib/agent/smith.rb
+++ lib/agent/smith.rb
@@ -6,10 +6,18 @@ module Agent
   module Smith
     class CLI < Thor
       desc "file_by_name [from_directory, to_directory]", "Move files from one directory to another while creating sub directories based on name"
-      def file_by_name(from_directory, to_directory=nil)
+      def file_by_name(from_directory=nil, to_directory=nil, directory_name_size=3)
+        raise 'From Directory not included' unless from_directory
+        raise 'To Directory not included' unless to_directory
+        FileUtils.mkdir_p to_directory
         Dir.glob(File.join(from_directory, "**/*/", File::SEPARATOR)).push(from_directory).uniq.with_progress.each do |path|
           Dir.glob(File.join(path, "*")).with_progress.each do |file|
-            puts file
+            if File.file?(file) && basename = File.basename(file, ".*")
+              new_directory = [to_directory, basename.scan(/.{1,#{directory_name_size}}/).join('/')].join('/')
+              FileUtils.mkdir_p new_directory
+              new_file = [new_directory, File.basename(file)].join('/')
+              FileUtils.cp_r file, new_file
+            end
           end
         end
       end
