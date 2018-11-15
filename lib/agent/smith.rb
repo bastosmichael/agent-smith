134fdea70bdab429c525129fecab0f0dbd2d326b
Michael Bastos <bastosmichael@gmail.com>
2018-11-15 16:31:43 -0600

    adding with progress

--- lib/agent/smith.rb
+++ lib/agent/smith.rb
@@ -1,15 +1,16 @@
 require "agent/smith/version"
 require 'thor'
+require 'progress'
 
 module Agent
   module Smith
     class CLI < Thor
-      desc "hello [name]", "say my name"
-      def hello(name)
-        if name == "Heisenberg"
-          puts "you are goddman right"
-        else
-          puts "say my name"
+      desc "file_by_name [from_directory, to_directory]", "Move files from one directory to another while creating sub directories based on name"
+      def file_by_name(from_directory, to_directory=nil)
+        Dir.glob(File.join(from_directory, "**/*/", File::SEPARATOR)).push(from_directory).uniq.with_progress.each do |path|
+          Dir.glob(File.join(path, "*")).with_progress.each do |file|
+            puts file
+          end
         end
       end
     end
