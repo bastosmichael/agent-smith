fbf30cb520a5946c6750d384503353e28bbcd799
Michael Bastos <bastosmichael@gmail.com>
2019-11-09 20:05:33 -0600

    adding csv to json

--- lib/agent/smith.rb
+++ lib/agent/smith.rb
@@ -1,6 +1,9 @@
 require "agent/smith/version"
 require 'thor'
 require 'progress'
+require 'fileutils'
+require 'smarter_csv'
+require 'json'
 
 module Agent
   module Smith
@@ -17,6 +20,31 @@ module Agent
               FileUtils.mkdir_p new_directory
               new_file = [new_directory, File.basename(file)].join('/')
               FileUtils.cp_r file, new_file
+            end
+          end
+        end
+      end
+
+      desc "csv_to_json [from_directory, to_directory, include_file_name]", "Create JSON files from CSV in one directory to another while creating sub directories based on file name"
+      def csv_to_json(from_directory=nil, to_directory=nil, include_file_name = true)
+        raise 'From Directory not included' unless from_directory
+        raise 'To Directory not included' unless to_directory
+        FileUtils.mkdir_p to_directory
+        Dir.glob(File.join(from_directory, "**/*/", File::SEPARATOR)).uniq.with_progress.each do |path|
+          relative_path = path.dup.tap{|s| s.slice!(from_directory)}
+          Dir.glob(File.join(path, "*.csv")).with_progress.each do |file|
+            if File.file?(file) && basename = File.basename(file, ".*")
+              array_of_records = SmarterCSV.process(file).uniq
+              next unless array_of_records.any?
+
+              new_directory = include_file_name ? [to_directory, relative_path, basename].join : [to_directory, relative_path].join
+              FileUtils.mkdir_p new_directory
+
+              array_of_records.uniq.with_progress.each do |record|
+                name = record[:hash] + '.json'
+                new_file = [new_directory, name].join('/')
+                File.write(new_file, record.to_json)
+              end
             end
           end
         end
